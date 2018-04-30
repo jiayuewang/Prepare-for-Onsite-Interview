a82dab78853e56610bc3444892c2a33c67f250b3
jiayue wang <jiayue3@illinois.edu>
2018-04-30 16:52:25 -0500

    ~

---
+++ lintcode_433.java
@@ -0,0 +1,80 @@
+class UnionFind { 
+
+    private int[] father = null;
+    private int count;
+
+    private int find(int x) {
+        if (father[x] == x) {
+            return x;
+        }
+        return father[x] = find(father[x]);
+    }
+
+    public UnionFind(int n) {
+        // initialize your data structure here.
+        father = new int[n];
+        for (int i = 0; i < n; ++i) {
+            father[i] = i;
+        }
+    }
+
+    public void connect(int a, int b) {
+        // Write your code here
+        int root_a = find(a);
+        int root_b = find(b);
+        if (root_a != root_b) {
+            father[root_a] = root_b;
+            count --;
+        }
+    }
+        
+    public int query() {
+        // Write your code here
+        return count;
+    }
+    
+    public void set_count(int total) {
+        count = total;
+    }
+}
+
+public class Solution {
+    /**
+     * @param grid a boolean 2D matrix
+     * @return an integer
+     */
+    public int numIslands(boolean[][] grid) {
+       int count = 0;
+       int n = grid.length;
+       if(n == 0) return 0;
+       int m = grid[0].length;
+       if(m == 0) return 0;
+       
+        UnionFind union_find = new UnionFind(n * m);
+       //上面自定义了class 
+        int total = 0;
+        for(int i = 0;i < grid.length; ++i)
+            for(int j = 0;j < grid[0].length; ++j)
+            if (grid[i][j])
+                total ++;
+    
+        union_find.set_count(total);
+        for(int i = 0;i < grid.length; ++i)
+            for(int j = 0;j < grid[0].length; ++j)
+            if (grid[i][j]) {
+                if (i > 0 && grid[i - 1][j]) {
+                    union_find.connect(i * m + j, (i - 1) * m + j);
+                }
+                if (i <  n - 1 && grid[i + 1][j]) {
+                    union_find.connect(i * m + j, (i + 1) * m + j);
+                }
+                if (j > 0 && grid[i][j - 1]) {
+                    union_find.connect(i * m + j, i * m + j - 1);
+                }
+                if (j < m - 1 && grid[i][j + 1]) {
+                    union_find.connect(i * m + j, i * m + j + 1);
+                }
+            }
+        return union_find.query();
+    }
+}
---
+++ lintcode_Minimum Size Subarray Sum.java
@@ -0,0 +1,22 @@
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+        // write your code here
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+       
+        for ( int l = 0, r = 0; r < nums.length; r++ ) {
+            sum += nums[r];
+            
+            while (sum >= s) {
+                ans = Math.min(ans, r - l + 1);
+                sum -= nums[l++];
+            }
+        }
+        return ans == Integer.MAX_VALUE? -1 : ans;
+    }
+}
